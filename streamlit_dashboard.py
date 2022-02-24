ab27234f6537d5b7db930be40cedfb384a442025
Srijith Rajamohan <sjster@gmail.com>
2022-02-24 12:16:42 -0500

    Slider added, can filter by slider range

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -38,8 +38,24 @@ df = pd.read_json('processed_issues.json')
 df = process_pull_requests(df)
 mlflow_metrics = get_mlflow_metrics()
 
+st.title("GitHub stats for MLflow")
+st.subheader("All issues ")
 st.dataframe(df)
 
+# ------------------- Filter based on slider values --------------------#
+date_vals = st.slider(
+     "Date range: please drag the slider to the desired range",
+     df['created_at'].min().to_pydatetime(),
+     df['created_at'].max().to_pydatetime(),
+     ( df['created_at'].min().to_pydatetime(), df['created_at'].max().to_pydatetime()),
+     step = pd.Timedelta(days=15)
+     )
+
+print("Slider values ",date_vals[0])
+
+df = df[df['created_at'] > date_vals[0]]
+
+# ------------------- Metrics ----------------- #
 col_metric1, col_metric2, col_metric3, col_metric4 = st.columns([1,1,1,1])
 with col_metric1:
     st.metric('Number of issues',len(df))
@@ -53,17 +69,33 @@ with col_metric3:
 with col_metric4:
     st.metric('Watchers',mlflow_metrics['watchers'])
 
-st.text('Issue date range ' + str(df['created_at'].min()) + ' ----> ' + str(df['created_at'].max()))
+# -------------------- Bugs, issues, enhancements, comments over time ------------------ #
 
-date_vals = st.slider(
-     "Date range:",
-     min_value = df['created_at'].min().to_pydatetime(),
-     max_value = df['created_at'].max().to_pydatetime(),
-     value = df['created_at'].max().to_pydatetime(),
-     step = pd.Timedelta(days=15)
-     )
+col_bugs, col_issues = st.columns([1,1])
 
-print("Slider values ",date_vals)
+with col_issues:
+    issues_by_week = df.set_index('created_at').resample(AGGREGATION_PERIOD).count()['id']
+    issues_by_week.rename('Issues', inplace=True)
+    fig = px.area(issues_by_week, title='Number of issues and comments by month')
+
+    comments_by_week = df.set_index('created_at').resample(AGGREGATION_PERIOD).agg({'comments': 'sum'})
+    fig2 = px.area(comments_by_week, title='Number of comments by month')
+    fig.add_trace(fig2['data'][0])
+    fig['data'][0].line.color = 'red'
+    st.plotly_chart(fig)
+
+labels_exploded = df.explode('labels')[['created_at','labels']]
+with col_bugs:
+    fig = px.area(resample_based_on_label('bug'), title="Bug/enhancements/integrations by month")
+    fig2 = px.area(resample_based_on_label('enhancement'), title="Bugs over time")
+    fig3 = px.area(resample_match_label_pattern('integration'))
+    fig.add_trace(fig2['data'][0])
+    fig.add_trace(fig3['data'][0])
+    fig['data'][0].line.color ='red'
+    fig['data'][1].line.color = 'green'
+    st.plotly_chart(fig)
+
+# --------------------- Pie charts fpr state, locked and PRs --------------------#
 
 col1, col2, col3 = st.columns([1,1,1])
 
@@ -77,35 +109,16 @@ with col2:
 
 with col3:
     res = df['processed_pull_request'].value_counts()
-    print(res)
     st.plotly_chart(px.pie(values=res.values, labels=res.index, title='Pull requests as % of total number of issues '))
 
-col_hist, col_issues = st.columns([1,1])
+# ---------------------- Histogram of issue labels and comments -------------------- #
+
+col_labels, col_hist = st.columns([1,1])
+
+with col_labels:
+    label_count = df.explode('labels')['labels'].value_counts()
+    st.plotly_chart(px.bar(label_count, title='Distribution of issue labels') )
+
 with col_hist:
     comments_hist = px.histogram(df, 'comments', title="Distribution of comments")
     st.plotly_chart(comments_hist)
-
-with col_issues:
-    issues_by_week = df.set_index('created_at').resample(AGGREGATION_PERIOD).count()['id']
-    issues_by_week.rename('Issues', inplace=True)
-    fig = px.area(issues_by_week, title='Number of issues and comments by month')
-
-    comments_by_week = df.set_index('created_at').resample(AGGREGATION_PERIOD).agg({'comments': 'sum'})
-    fig2 = px.area(comments_by_week, title='Number of comments by month')
-    fig.add_trace(fig2['data'][0])
-    fig['data'][0].line.color = 'red'
-    st.plotly_chart(fig)
-
-label_count = df.explode('labels')['labels'].value_counts()
-st.plotly_chart(px.bar(label_count, title='Distribution of issue labels') )
-
-labels_exploded = df.explode('labels')[['created_at','labels']]
-
-fig = px.area(resample_based_on_label('bug'), title="Bugs,enhancements,integrations over time")
-fig2 = px.area(resample_based_on_label('enhancement'), title="Bugs over time")
-fig3 = px.area(resample_match_label_pattern('integration'))
-fig.add_trace(fig2['data'][0])
-fig.add_trace(fig3['data'][0])
-fig['data'][0].line.color ='red'
-fig['data'][1].line.color = 'green'
-st.plotly_chart(fig)
