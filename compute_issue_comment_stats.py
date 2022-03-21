2b85d81c89227e0ed5c32aae85748859f7de9232
Srijith Rajamohan <sjster@gmail.com>
2022-03-21 19:44:17 -0400

    Timestamp issue fixed

--- compute_issue_comment_stats.py
+++ compute_issue_comment_stats.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from dateutil import parser
+import pytz
 import pickle
 
 def get_data():
@@ -18,6 +19,7 @@ def compute_stats():
         comment_dates_parsed = elem[3]['comment_dates'][pos]
         issue_date = elem[2]
         first_comment_time = parser.parse(comment_dates_parsed[0])
+        first_comment_time = first_comment_time.replace(tzinfo=pytz.utc)
         issue_id_list.append(elem[0])
         issue_date_list.append(issue_date)
         time_first_response_list.append(first_comment_time - issue_date)
